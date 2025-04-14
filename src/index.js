389c23edf29903e8953f795fc48edc5b7a9922fe
Sam Oberly <samob917@outlook.com>
2025-04-14 22:27:38 +0100

    Add webpack configuration and initial files
    Initial files (index and template) are added and work. Next will be
    styling and adding functionality. I will need to make the code more
    readable and break it out into components.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const content = document.querySelector(".content");
+
+const projectButton = document.createElement("button");
+projectButton.textContent = "Create New Project";
+
+content.appendChild(projectButton);
